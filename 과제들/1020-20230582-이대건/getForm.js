7a57a126203c43d1477b00bbc22fe8a9c49ee86d
SuperSlowStarter <cleangabby@gmail.com>
2024-03-02 04:12:15 +0900

    initaial commit

---
+++ 과제들/1020-20230582-이대건/getForm.js
@@ -0,0 +1,6 @@
+const selectMenu = document.querySelector('#major');
+function displaySelect() {
+    let selectedText = selectMenu.options[selectMenu.selectedIndex].innerText;
+    alert(`[${selectedText}] 를 선택했습니다.`);
+}
+selectMenu.onchange = displaySelect;
